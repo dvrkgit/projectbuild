8a58c2f40df9d4997e04e02ad61dbce77a63b19a
Venkat <ravikumar.venkat@gmail.com>
2025-12-22 14:32:03 +0530

    Third

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Jenkins Build!</h1>");
+    }
+}
